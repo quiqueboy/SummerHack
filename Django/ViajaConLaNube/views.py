60fe6d552458ea98f1c440afe4f976a93fc07fd4
quiqueboy <quiquebboy2010@gmail.com>
2021-07-31 17:01:50 -0500

    ya funciona el mapa en la carpeta template

--- Django/ViajaConLaNube/views.py
+++ Django/ViajaConLaNube/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+import folium
 
 # Create your views here.
 def inicio(request):
     return render(request, 'index.html')
 
 def metro(request):
+    m=folium.Map(location=[])
     return render(request, 'metro.html')
 
 def metrobus(request):
